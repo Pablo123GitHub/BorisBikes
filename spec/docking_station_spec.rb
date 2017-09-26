edc2127e501105323920e7827aa9225c7ac4cc47
Pablo <pabloandresvdl@gmail.com>
2017-09-26 17:56:42 +0100

    add test for release_bike method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,4 +3,14 @@ describe DockingStation do
   it "expects to respond to the release_bike method" do
   expect(DockingStation.new).to respond_to(:release_bike)
   end
+  it "expects to get a bike upon calling release_bike method " do
+    expect(DockingStation.new.release_bike).to be_instance_of(Bike)
+
+  end
+
+  it "expects the bike to be working " do
+    expect(DockingStation.new.release_bike.working?).to be_instance_of(Bike)
+
+  end
+
 end
