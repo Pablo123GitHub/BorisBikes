7fb629a9c277710dd8b11f91dae1658c6dcf080e
Pablo <pabloandresvdl@gmail.com>
2017-09-26 16:07:36 +0100

    add respond_to test to rspec file

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,6 @@
 require './lib/docking_station.rb'
 describe DockingStation do
-
+  it "expects to respond to the release_bike method" do
+  expect(DockingStation.new).to respond_to(:release_bike)
+  end
 end
