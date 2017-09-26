15b617688c6045bfa4dc8e82b052f4234ae6c11e
Alexis Carlier <alexis.carlier0@gmail.com>
2017-09-26 16:30:18 +0100

    Added Bike.rb

--- spec/Bike_spec.rb
+++ spec/Bike_spec.rb
@@ -1,7 +1,7 @@
-# require etc
+require './lib/Bike.rb'
 
 describe 'Bike' do
   it 'checks if a Bike is working' do
-    expect(Bike.new).to respond_to(:working)
+    expect(Bike.new).to respond_to(:working?)
   end
 end
