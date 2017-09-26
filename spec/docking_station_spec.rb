f3179929eee2f2b598b8f801b1e60c11b3c787ce
Pablo <pabloandresvdl@gmail.com>
2017-09-26 21:27:52 +0100

    update test with 'subject' keyword instead of DockingStation.new

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,15 +1,15 @@
 require './lib/docking_station.rb'
 describe DockingStation do
   it "expects to respond to the release_bike method" do
-  expect(DockingStation.new).to respond_to(:release_bike)
+  expect(subject).to respond_to(:release_bike)
   end
   it "expects to get a bike upon calling release_bike method " do
-    expect(DockingStation.new.release_bike).to be_instance_of(Bike)
+    expect(subject.release_bike).to be_instance_of(Bike)
 
   end
 
   it "expects the bike to be working " do
-    expect(DockingStation.new.release_bike.working?).to eq true
+    expect(subject.release_bike.working?).to eq true
 
   end
 
