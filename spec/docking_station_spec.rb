758d29bbbf0a8dbd39bea2ac1569150248388f1b
Pablo <pabloandresvdl@gmail.com>
2017-09-28 23:04:51 +0100

    fix number of times: 20 instead of 19 in test to check error when full

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -18,9 +18,8 @@ describe DockingStation do
 
   describe '#dock' do
     it 'raises an error when the docking station is full' do
-      19.times do
-        subject.dock(Bike.new)
-      end 
+      20.times {subject.dock(Bike.new)}
+
       expect { subject.dock(Bike.new)}.to raise_error 'DockingStation is full'
     end
   end
